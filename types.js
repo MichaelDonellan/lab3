5d3fefa71bcf464574a665ebaa59ed75d379d790
Michael <you@example.commichaeldonellan1999@gmail.com>
2021-02-03 15:09:52 +0000

    lab upload

---
+++ types.js
@@ -0,0 +1,8 @@
+var myNumber = 55;
+console.log("MyNumber is: " + myNumber);
+var myBool = false;
+console.log(myBool);
+var list = ["hello", "3", "false"];
+for (var i = 0; i < list.length; i++) {
+    console.log("String " + (i + 1) + " is " + list[i]);
+}
---
+++ types.ts
@@ -0,0 +1,11 @@
+let myNumber: number =55;
+console.log("MyNumber is: " + myNumber);
+
+let myBool:boolean = false;
+console.log(myBool);
+
+let list:Array<string> = ["hello", "3","false"]
+for(let i=0;i<list.length; i++){
+
+    console.log("String " + (i+1) + " is " +list[i])
+}
